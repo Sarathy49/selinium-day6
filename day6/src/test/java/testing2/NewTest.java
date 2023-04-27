5b2b93dbe567015636af232359cfd1271c9a5d6b
Sarathy49 <119049025+Sarathy49@users.noreply.github.com>
2023-04-27 16:03:58 +0530

    Add files via upload

---
+++ day6/src/test/java/testing2/NewTest.java
@@ -0,0 +1,34 @@
+package testing2;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class NewTest{
+    int a=20;int b=20;
+    int c;
+    @Test
+    public void add() {
+        c=a+b;
+        System.out.print(c);
+        Assert.assertEquals(c, 40);
+    }
+    @Test
+    public void sub() {
+        c=a-b;
+        System.out.print(c);
+        Assert.assertEquals(c, 0);
+    }
+    @Test
+    public void mul() {
+        c=a*b;
+        System.out.print(c);
+        Assert.assertEquals(c, 400);
+    }
+    @Test
+    public void div() {
+        c=a/b;
+        System.out.print(c);
+        Assert.assertEquals(c, 1);
+    }
+
+}
